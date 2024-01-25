10358ca9c62b079808b3caa2d33617e5629951c1
Pyry Lahtinen <pyry@pyry.info>
2024-01-25 16:05:14 +0200

    Initial Commit

---
+++ learnpi/learnpiApp.swift
@@ -0,0 +1,17 @@
+//
+//  learnpiApp.swift
+//  learnpi
+//
+//  Created by Pyry Lahtinen on 25.1.2024.
+//
+
+import SwiftUI
+
+@main
+struct learnpiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
